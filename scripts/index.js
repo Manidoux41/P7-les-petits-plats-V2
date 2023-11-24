0116a02f5519e2d63083fa843a5c10a6224cc112
Manidoux41 <lherissondu41@gmail.com>
2023-11-24 08:07:43 +0100

    add for loop

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+function createRecipeCard(recipe) {
+    const recipeDiv = document.createElement('div');
+    recipeDiv.classList.add('recipe');
+  
+    const recipeImg = document.createElement('img');
+    recipeImg.src = recipe.image;
+    recipeImg.alt = recipe.name;
+    recipeDiv.appendChild(recipeImg);
+  
+    const recipeName = document.createElement('h3');
+    recipeName.textContent = recipe.name;
+    recipeDiv.appendChild(recipeName);
+  
+    // Add more elements as needed
+  
+    return recipeDiv;
+  }
+  
+  function displayRecipes(recipes) {
+    const searchResults = document.querySelector('.recipes-card-container');
+    searchResults.innerHTML = '';
+  
+    for (let index = 0; index < recipes.length; index++) {
+      const recipe = recipes[index];
+      const recipeCard = createRecipeCard(recipe);
+      searchResults.appendChild(recipeCard);
+    }
+  }
+  
+  // Usage
+  displayRecipes(recipes);
